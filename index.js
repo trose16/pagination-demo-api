918a51881916d26e416fff8ec26faa8dff024f14
T Warfield-Graham <twarfield-graham@Mac.lan>
2025-08-05 21:09:22 -0600

    Adds info on pagination to readme

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ const PORT = process.env.PORT || 3000;
 
 
 // Adding a simple "hello world" route to confirm the server is running.
-
 app.get('/', (req, res) => {
     res.send('Hello from the Pagination Demo API!');
 });
 
+// Simple rout to return all products
+app.get('/products', (req, res) => {
+    res.json(products);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
